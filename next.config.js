2291d89d08054236a79e28fa0670726f7a9356f6
rudrodip <official.rudrodipsarker@gmail.com>
2023-09-14 22:25:36 +0600

    youtube channel mage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
