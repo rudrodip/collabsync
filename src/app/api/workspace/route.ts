1f82a3faeb864c55547e64108dd62796580926b8
rudrodip <official.rudrodipsarker@gmail.com>
2023-09-14 22:29:24 +0600

    workspace api

---
+++ src/app/api/workspace/route.ts
@@ -0,0 +1,35 @@
+import { env } from "@env.mjs";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const raw = true;
+  const token = await getToken({ req, raw });
+  const reqBody = await req.json();
+
+  if (token) {
+    try {
+      const res = await fetch(
+        `${env.BACKEND_API_URL}/workspace`,
+        {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": token.toString(),
+          },
+          body: JSON.stringify(reqBody)
+        }
+      );
+
+      const data = await res.json();
+      
+      return NextResponse.json(data);
+    } catch (error) {
+      console.error('Error:', error);
+      return new Response("Internal Server Error", { status: 500 });
+    }
+  } else {
+    // Not Signed in
+    return new Response("Unauthorized", { status: 401 });
+  }
+}
